eb16cbcdee3dc3716af8d55e0b2dbac542ba9739
rjohnston <rpjohnston@gmail.com>
2018-01-04 14:52:22 -0500

    'Fixed' sig solve recursive

--- sigSolver_recursive.py
+++ sigSolver_recursive.py
@@ -20,6 +20,10 @@ class WORD(object):
 	def validSet(self,pDict):
 		# Takes a partial dictionary, and checks to see if it has any words that go with it:
 		#print("\tIn ",self.index,"validSet")
+		
+		# Reset self.signatures- really, really stupid error here
+		(self.signatures,self.lookup) = get_sig_sel(self.encWord,self.sChars,self.matches)
+
 		sets = []
 		for key in pDict:
 			# Do we have that key:
@@ -50,7 +54,8 @@ class WORD(object):
 
 		self.signatures = valid
 
-#{'T': 'L', 'Z': 'E', 'N': 'P', 'W': 'O', 'J': 'A', 'S': 'T', 'A': 'I', 'L': 'N', 'E': 'J'}
+	#def restore_signatures()
+#
 class SIGSOLVER(object):
 	# TODO: Set this up to run with a collection of words.  Setup should only occur once
 
@@ -94,11 +99,11 @@ class SIGSOLVER(object):
 		# Add the selected signature to the current dictionary
 		inUse = set(usedWords)
 		inUse.add(index)
-		print("*********************************************")
-		print("new pick next call",index,"len",len(self.WORDS[index].signatures))
-		print()
+		##print("*********************************************")
+		##print("new pick next call",index,"len",len(self.WORDS[index].signatures))
+		##print()
 		for signature in self.WORDS[index].signatures:
-			print("signature:",signature,index)
+			##print("signature:",signature,index)
 			sigValid = True
 			pDict = deepcopy(inDict)
 			uvals = deepcopy(mappedChars)
@@ -121,10 +126,10 @@ class SIGSOLVER(object):
 						break
 
 			
-			print("pDict",pDict)
+			##print("pDict",pDict)
 
 			if sigValid == False:
-				print("\tHere?")
+				##print("\tHere?")
 				continue
 			# confirm that every WORD has a set of valid entries for this dictionary:
 			for curr in self.WORDS:
@@ -133,7 +138,7 @@ class SIGSOLVER(object):
 					pass
 				else:
 					# Fault detected, reject this pDict
-					print("\tSignature rejected, no valid sets for ",curr.encWord,curr.index)
+					##print("\tSignature rejected, no valid sets for ",curr.encWord,curr.index)
 					sigValid = False
 					break
 
@@ -141,7 +146,7 @@ class SIGSOLVER(object):
 				# This one is good, pick the next one/verify we aren't already done
 				if len(inUse) == len(self.WORDS):
 					# Really Done
-					print("DONE!")
+					##print("DONE!")
 					for word in self.WORDS:
 						print(word.validMatches)
 					print(pDict)
@@ -167,13 +172,13 @@ class SIGSOLVER(object):
 								for x in tst:
 									print(x.signatures)
 								print(pDict)
-								print("\tSignatures invalidated!")
+								##print("\tSignatures invalidated!")
 								for word in self.WORDS:
 									print(word.index, len(word.signatures))
 								break
 
 							self.pickNext(item.index,inUse,pDict,uvals)
-							print("\t***********RETURNING UP ONE",index)
+							##print("\t***********RETURNING UP ONE",index)
 							# Reset the WORD OBJECTS to their prior state:
 							for curr in self.WORDS:
 								curr.validSet(pDict)
@@ -185,195 +190,6 @@ class SIGSOLVER(object):
 					#	else:
 					#		break
 					#self.pickNext(tst[xx].index,inUse,pDict,uvals)
-
-
-		# If there are words left unpicked, call pickNext again
-		#PEOPLE LEA E CO F  OSEF AN   ASIL  TO COIN ANTI PIPELINE  ASP
-		#NZWNTZ TZJQZ EWDR VWPZR JLG OJPATC SW EWAL JLSA NANZTALZ KJPN
-		#PEOPLE LEAVE JOBS HOMES AND FAMILY TO JOIN ANTI PIPELINE CAMP
-
-#signature: AINT 8
-#pDict 	{'T': 'L', 'L': 'N', 'N': 'P', 'W': 'O', 'J': 'A', 'S': 'T', 'Z': 'E', 'A': 'I'}
-#		{'T': 'L', 'L': 'N', 'N': 'P', 'W': 'O', 'J': 'A', 'S': 'T', 'Z': 'E', 'A': 'I'}
-#        Signatures invalidated!
-
-'''
-	def sigTrim(self, tar):
-		# tar is the wordlist to trim
-
-		# this is a set of matches that are invalid due to insignificant character
-		# mappings
-		self.shitSet = set()
-		self.tar = tar
-		# Get the list of significant characters for selected word:
-		selSchars = list(get_shared_chars(self.words, tar))
-
-		# Get the dict of signatures for the selected word
-		# selSigs: Keys="clrSig", Values=[matches with that clrSig]
-		self.selSigs = get_sig_sel(self.words[tar],selSchars, self.matches[tar])
-
-		# Run find valid signatures:
-		ret = self.find_valid_signatures(selSchars, self.selSigs.keys(), self.charDicts)
-
-		# Clean out any matches on the shit list and return the list:
-
-		# Ret is a list of valid signatures, combine the ones that are OK
-		# and then fetch the valid matches
-		vindexes = []
-		out = []
-		#print(ret)
-
-		for vsig in ret:
-			for index in self.selSigs[vsig]:
-				if index not in self.shitSet:
-					out.append(self.matches[self.tar][index])
-
-		#print(out)
-
-		return out	
-
-	def find_valid_signatures(self, eSchars, sigsIn, sigDicts):
-		# TODO: Once it's easier to understand, try optimizing it.
-		# eSchars is a list of encoded shared characters from the target word
-		# Checks to see which signatures (if any) are valid
-		
-		validSigs = []
-		setOftChars = set(eSchars)
-
-		sigsIn = list(sigsIn)
-		# For every signature, establish a set of subsignatures that we need to apply 
-		# to each word:
-		# LIST [ DICTS ], where keys are sigTuples, and values are indexes of sigsIn that use
-		# that signature
-
-		uSigs = []
-		correspondingSigs = []
-		for i in range(0,len(sigDicts)):
-			if sigDicts[i] == None:
-				uSigs.append(None)
-				correspondingSigs.append(None)
-				continue
-
-			# Get the set of shared characters for this word:
-			sEncChars = set(sigDicts[i].keys())
-			sEncChars = sEncChars & setOftChars			
-			
-			# Pass if there are no shared chars
-			if len(sEncChars) == 0:
-				uSigs.append(None)
-				correspondingSigs.append(None)
-				continue
-
-
-			tDict = {}
-			tcsd = {}
-			for j in range(0,len(sigsIn)):
-				iTuple = tuple(get_sig_tuples(eSchars,sigsIn[j], sEncChars))
-				if iTuple in tDict:
-					# We've already seen this tuple set
-					tDict[iTuple].append(j)
-					tcsd[iTuple].append(sigsIn[j])
-				else:
-					# We haven't seen this tuple yet, establish a new list:
-					tDict[iTuple] = [j]
-					tcsd[iTuple]  = [sigsIn[j]]
-			
-			uSigs.append(tDict)
-			correspondingSigs.append(tcsd)
-		
-		# Run check intersections on all:
-		validSets = []
-		for i in range(0,len(sigDicts)):
-			if uSigs[i] == None:
-				validSets.append(None)
-				continue
-			if i == self.tar:
-				validSets.append(None)
-				continue
-
-			tSet = set()
-			for key in uSigs[i]:
-				#ret = self.check_intersections(key,sigDicts[i]):
-				#if ret != False:
-				
-				# merge the indexes for corresponding sigs-- this hurts my brain
-				#indexlist = []
-				#for A in correspondingSigs[i]:
-				#	for B in correspondingSigs[i][A]:
-				#		indexlist.append(z for z in self.selSigs[B])
-
-				#print("usigs",uSigs) 
-				#print("key is", key)
-				if self.check_intersections(key,sigDicts[i], correspondingSigs[i][key], i):
-					for x in uSigs[i][key]:
-						tSet.add(x)
-
-			validSets.append(tSet)
-
-		# Merge all sets:
-		out = set(x for x in range(0,len(sigsIn)))
-		for i in range(0,len(validSets)):
-			if validSets[i] != None:
-				out &= validSets[i]
-		
-		return [sigsIn[x] for x in out]
-
-	def check_intersections(self, mapping, sigDict, CS, sel2):
-		# tIndexes is the list of indexes assosciated with the current mapping.
-		# tar is the word that we're trimming, sel2 is the position that we're trimming
-		# given a mapping (a list of (enc, clr) tuples), and a sigDict
-		# Determine if the mapping is valid.  Returns true or false
-		# Recall: sigDict[encChar][clrChar] = set of match indexes
-		
-		sets = []	
-		try:		
-			for item in mapping:
-				sets.append(sigDict[item[0]][item[1]])
-		except KeyError:
-			# If a key error occurs, the mapping cannot be valid
-			return False
-
-		# MAKE A COPY OF THAT SET!!!
-		# That took an hour to debug
-		x = set(sets[0])
-		
-		for curr in sets:
-			x &= curr
-			if len(x) == 0:
-				return False
-
-		# Create a set of the clear characters to ignore		
-		toRemove = set(mapping[a][1] for a in range(0,len(mapping)))
-		tIndexes = []
-		
-		for A in CS:
-			for z in self.selSigs[A]:
-				tIndexes.append(z)
-
-		for index in tIndexes:
-			Y = set(self.matches[self.tar][index])
-			Y -= toRemove
-
-			found = False
-			for curr in x:
-				X = set(self.matches[sel2][curr])
-				X -= toRemove
-				
-				# Check for intersection of X & Y, if the intersection exists, 
-				# then that word is invalid
-				if len(X & Y) == 0:
-					# FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK
-					# GODDAMNMOTHERFUCKING == INSTEAD OF =. FUCK ME.
-					found = True
-					break
-
-			if found:
-				pass
-			else:
-				self.shitSet.add(index)
-		
-		return True
-'''
 
 def get_charDicts(encWords,matches):
 	# Returns a list containing the sigDicts for each word
